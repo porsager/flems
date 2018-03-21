e8842a90212a28474e9c184baee119414da40323
Rasmus Porsager <rasmus@porsager.com>
2018-03-21 23:18:31 +0100

    Replace rust svgcleaner dep with npm svgo fixes #37

--- scripts/svg.js
+++ scripts/svg.js
@@ -1,18 +1,16 @@
-const cp = require('child_process')
+const SVGO = require('svgo')
+
+const svgo = new SVGO()
 
 module.exports = function svg() {
   return {
     name: 'svg',
     transform: (code, id) => {
       if (id.endsWith('.svg')) {
-        return {
+        return svgo.optimize(code).then(result => ({
           map: { mappings: '' },
-          code: 'export default ' + JSON.stringify(cp.execSync(
-            'svgcleaner ' + id + ' -c', {
-              encoding: 'utf8'
-            }
-          ))
-        }
+          code: 'export default ' + JSON.stringify(result.data)
+        }))
       }
     }
   }
